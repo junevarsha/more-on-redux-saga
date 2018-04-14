5f4c423035776583a62bbd1227f02fb5ab08cecb
VARSHA THIRUMAKIL <varsht612@gmail.com>
2018-04-14 19:30:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Distributed Async calls with redux sagas [![Build Status](https://travis-ci.org/andresmijares/async-redux-saga.svg?branch=master)](https://travis-ci.org/andresmijares/async-redux-saga)
+
+A similar approach to [Flattering Promise Chains](http://solutionoptimist.com/2013/12/27/javascript-promise-chains-2/) using redux sagas.
+
+Check the final demo [here](http://async-redux-saga.surge.sh/).
+
+Check the article [here](https://medium.com/@andresmijares25/async-operations-using-redux-saga-2ba02ae077b3#.sxsygk28s)
+
+## Installation
+
+1. Clone the repo
+2. Run `npm install`
+3. Run `npm start`
+4. Find way to make it better, and pull request!
+
+## License
+[CC-BY](https://creativecommons.org/licenses/by/3.0/)
---
+++ src/sagas/apiCalls.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+import TravelServiceApi from '../api/travelServiceApi';
+
+export const loadUser = () => {
+  console.log('loading user');
+  const userVal = TravelServiceApi.getUser().then(res => res);
+  console.log("hjjk",userVal);
+  return TravelServiceApi.getUser().then(res => res);
+};
+
+export const loadDeparture = (user) => {
+  console.log('loading departure');
+  return TravelServiceApi.getDeparture(user).then(res => res);
+};
+
+export const loadFlight = (flightID) => {
+  console.log('loading flight');
+  return TravelServiceApi.getFlight(flightID).then(res => res);
+};
+
+export const loadForecast = (date) => {
+  console.log('loading forecast');
+  return TravelServiceApi.getForecast(date).then(res => res);
+};
